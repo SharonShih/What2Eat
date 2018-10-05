29505bb44306a3c0970e7861295f5631eff81937
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2018-10-05 16:07:16 -0700

    reg form

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000000',
+    backgroundColor: '#2E86C1',
     paddingLeft: 60,
     paddingRight: 60,
   },
--- app/components/RegForm.js
+++ app/components/RegForm.js
@@ -14,9 +14,12 @@ export default class RegForm extends Component<Props> {
             <View style = {styles.RegForm}>
                 <Text style={styles.header}> Registration </Text>
                 <TextInput style={styles.input} placeholder = "first name"></TextInput>
-                <TextInput></TextInput>
-                <TextInput></TextInput>
-                <TextInput></TextInput>
+                <TextInput style={styles.input} placeholder = "last name"></TextInput>
+                <TextInput style={styles.input} placeholder = "email"></TextInput>
+                <TextInput style={styles.input} placeholder = "password"></TextInput>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Sign Up!</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -32,12 +35,31 @@ const styles = StyleSheet.create({
         color: "#FFF",
         paddingBottom: 10,
         marginBottom: 40,
-        borderBottomColor: "#2D911E",
+        borderBottomColor: "#000000",
+        borderBottomWidth: 1,
+        fontWeight: 'bold',
 
     },
     input: {
         alignSelf: 'stretch',
         fontSize: 18,
         color: "#FFF",
+        marginBottom: 30,
+        height: 40,
+        borderBottomColor: "#FFF",
+        borderBottomWidth: 1,
+    },
+    button: {
+        backgroundColor: "#000000",
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        padding: 20,
+        marginTop: 30,
+
+    },
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 18,
     }
 });
