329d28aa09bf28c3087f96fb68d1a9b3f2e61bb9
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2018-12-05 20:41:10 -0800

    searchDisplayPage DONE

--- app/scenes/SearchDisplayPage.js
+++ app/scenes/SearchDisplayPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {YelpAuth} from '../../services/YelpFusion';
 import {Alert, ImageBackground, Image, StyleSheet, Text, View, Button, Linking, TouchableHighlight} from "react-native";
 import {Header, Icon, Left} from "native-base";
+import Firebase from '../../services/Firebase';
 
 export default class YelpSearchRequest extends Component<Props> {
   static navigationOptions = {
@@ -10,11 +11,11 @@ export default class YelpSearchRequest extends Component<Props> {
       backgroundColor: '#7174BF',
       marginTop: 15,
       borderBottomWidth: 0,
-      opacity:0.7,
+      opacity: 0.7,
     },
     headerTintColor: '#fff',
     headerTitleStyle: {
-      fontSize:20,
+      fontSize: 20,
       textAlign: 'left',
 
     },
@@ -66,73 +67,105 @@ export default class YelpSearchRequest extends Component<Props> {
 
   }
 
-render()
-{
-  if (this.state.isLoading) {
-    return (
-      <View style={styles.container}>
-      </View>
-    )
-  } else {
-    let name = this.state.dataSource[this.state.randomNum].name;
-    let image_url = this.state.dataSource[this.state.randomNum].image_url;
-    let categoriesRef = this.state.dataSource[this.state.randomNum].categories;
-    let categories = [];
-    for (let i = 0; i< categoriesRef.length ; i ++) {
-      categories.push(categoriesRef[i].title);
-    }
-    let location = this.state.dataSource[this.state.randomNum].location.display_address;
-    let rating = this.state.dataSource[this.state.randomNum].rating;
-    let display_phone = this.state.dataSource[this.state.randomNum].display_phone;
-    console.log(categories);
-    let coordinates = this.state.dataSource[this.state.randomNum].coordinates.latitude + "+" + this.state.dataSource[this.state.randomNum].coordinates.longitude;
-    //TODO: GOOGLE MAP
-    // let google = "google.navigation:q=" + coordinates;
-    let apple = "maps://app?daddr=" + coordinates;
-    return (
-      <ImageBackground source={require('../components/Stellar.png')}
-                       style={styles.Background}>
-        <Text style = {styles.Text}>What To Eat Today{"\n"}&#8595; &#8595; &#8595; &#8595;</Text>
-        <Image source={{uri: image_url}}
-               style={styles.image}/>
-          <View style = {styles.shadowOffset}>
-            <Text style = {styles.textName}>{name}</Text>
-            <Text style = {styles.textInfo}>{categories.toString()}</Text>
-            <Text style = {styles.textInfo}>&#10147; {location}</Text>
-            <Text style = {styles.textInfo}>&#9733; {rating}/5 Stars </Text>
-            <Text style = {styles.textInfo}>&#9990; {display_phone} </Text>
+  goPlace(url) {
+    let uid = Firebase.auth().currentUser.uid;
+    let db = Firebase.firestore(Firebase);
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    let docRef = db.collection("users").doc(uid);
+    docRef.get().then(function (doc) {
+      if (doc.exists) {
+        let tempArray = [];
+        let field = doc.get('visited_restaurant');
+        for (let index = 0; index < field.length; index++) {
+          tempArray.push(field[index]);
+        }
+        tempArray.push(this.state.dataSource[this.state.randomNum]);
+        docRef.update(
+          {
+            visited_restaurant: tempArray,
+          }
+        ).then(function () {
+          console.log("Document successfully written!");
+        })
+          .catch(function (error) {
+            console.error("Error writing document: ", error);
+          });
+      } else {
+        console.log("doesn't exist")
+      }
+    }.bind(this)).catch(function (error) {
+      console.log("Error getting document:", error);
+    })
+    this.props.navigation.goBack();
+    Linking.openURL(url);
+  }
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+        </View>
+      )
+    } else {
+      let name = this.state.dataSource[this.state.randomNum].name;
+      let image_url = this.state.dataSource[this.state.randomNum].image_url;
+      let categoriesRef = this.state.dataSource[this.state.randomNum].categories;
+      let categories = [];
+      for (let i = 0; i < categoriesRef.length; i++) {
+        categories.push(categoriesRef[i].title);
+      }
+      let location = this.state.dataSource[this.state.randomNum].location.display_address;
+      let rating = this.state.dataSource[this.state.randomNum].rating;
+      let display_phone = this.state.dataSource[this.state.randomNum].display_phone;
+      let coordinates = this.state.dataSource[this.state.randomNum].coordinates.latitude + "+" + this.state.dataSource[this.state.randomNum].coordinates.longitude;
+      //TODO: GOOGLE MAP
+      // let google = "google.navigation:q=" + coordinates;
+      let apple = "maps://app?daddr=" + coordinates;
+      return (
+        <ImageBackground source={require('../components/Stellar.png')}
+                         style={styles.Background}>
+          <Text style={styles.Text}>What To Eat Today{"\n"}&#8595; &#8595; &#8595; &#8595;</Text>
+          <Image source={{uri: image_url}}
+                 style={styles.image}/>
+          <View style={styles.shadowOffset}>
+            <Text style={styles.textName}>{name}</Text>
+            <Text style={styles.textInfo}>{categories.toString()}</Text>
+            <Text style={styles.textInfo}>&#10147; {location}</Text>
+            <Text style={styles.textInfo}>&#9733; {rating}/5 Stars </Text>
+            <Text style={styles.textInfo}>&#9990; {display_phone} </Text>
           </View>
 
-        <View style = {{flexDirection: 'row', flexWrap: 'wrap'}}>
-          <TouchableHighlight
-            style ={styles.goButton}>
-            <Button
-              title="Let's Go!"
-              color="#000099"
-              onPress={() => Linking.openURL(apple)} />
-          </TouchableHighlight>
+          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+            <TouchableHighlight
+              style={styles.goButton}>
+              <Button
+                title="Let's Go!"
+                color="#000099"
+                onPress={() => this.goPlace(apple)}/>
+            </TouchableHighlight>
 
-          <TouchableHighlight
-            style ={styles.nextButton}>
-            <Button
-              title="Next&#8594;"
-              color="#000"
-              onPress={() => this.randomChoice()} />
-          </TouchableHighlight>
-        </View>
-      </ImageBackground>
-    );
+            <TouchableHighlight
+              style={styles.nextButton}>
+              <Button
+                title="Next&#8594;"
+                color="#000"
+                onPress={() => this.randomChoice()}/>
+            </TouchableHighlight>
+          </View>
+        </ImageBackground>
+      );
+    }
   }
-}
 }
 
 const styles = StyleSheet.create({
 
-  Background:{
+  Background: {
     width: '100%',
     height: '100%',
   },
-  Text:{
+  Text: {
     fontSize: 30,
     fontFamily: 'Georgia',
     color: '#FFF',
@@ -144,7 +177,7 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width:  '100%',
+    width: '100%',
     height: 200,
     marginTop: 180,
 
@@ -170,7 +203,7 @@ const styles = StyleSheet.create({
     borderBottomColor: 'gray',
     borderTopColor: 'gray',
     borderBottomWidth: 1,
-    borderTopWidth:1,
+    borderTopWidth: 1,
     borderLeftWidth: 0,
     borderRightWidth: 0,
 
@@ -185,7 +218,7 @@ const styles = StyleSheet.create({
     //marginRight: 5,
     marginTop: 0,
   },
-  goButton:{
+  goButton: {
     height: 45,
     width: 180,
     borderRadius: 20,
@@ -194,7 +227,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 40,
   },
-  nextButton:{
+  nextButton: {
     height: 45,
     width: 100,
     borderRadius: 20,
