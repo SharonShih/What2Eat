f0e9a415bfd8689454a1dc14d41da8e3906600ba
benguo <benson777>
2019-11-09 15:33:40 -0800

    fix food images

--- app/scenes/ChooseFavorite.js
+++ app/scenes/ChooseFavorite.js
@@ -4,43 +4,21 @@ import FoodPicture from '../components/FoodPicture';
 import Firebase from '../../services/Firebase';
 import 'firebase/firestore';
 import {StyleSheet, View, Button, ImageBackground, TouchableHighlight,ScrollView} from "react-native";
+import JapaneseImag from '../image/japanese.jpg'
+import MexicanImag from '../image/mexican.jpg'
+import AmricanImag from '../image/american.jpg'
+import KoreanImag from '../image/korean.jpg'
+import FastFood from '../image/fastfood.jpg'
+import SeaFood from '../image/seafood.jpg'
+import ChineseFood from '../image/chinese.jpg'
+import ItalianFood from '../image/italian.jpg'
+import Vietnamese from '../image/vietnamese.jpg'
+import Steak from '../image/steak.jpg'
+import Cafe from '../image/cafe.jpg'
+import Desert from '../image/dessert.jpg'
 
-const JapaneseImag = {
-  uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Breakfast_at_Tamahan_Ryokan%2C_Kyoto.jpg/2560px-Breakfast_at_Tamahan_Ryokan%2C_Kyoto.jpg'
-}
-const MexicanImag = {
-  uri: 'https://marvel-live.freetls.fastly.net/canvas/2018/10/4415bb555f0f407a851d3c6faaddae15?quality=95&fake=.png'
-}
-const AmricanImag = {
-  uri: 'https://s3-us-west-2.amazonaws.com/ncs-ons10-us-west-2-159685838580-content-prd/ns1or_wt1_p/s3fs-public/styles/cf_medium_teaser_320x180/public/Image_cont_c925eca7fba207d470efc1999e422dee6440c41d.jpeg'
-}
-const KoreanImag = {
-  uri: 'https://i.kinja-img.com/gawker-media/image/upload/s--djYdkXK4--/c_scale,f_auto,fl_progressive,q_80,w_1600/be9vyovcn2gqwod7kd16.jpg'
-}
-const FastFood = {
-  uri: 'https://www.rd.com/wp-content/uploads/2017/10/15-13-things-your-fast-food-worker-won-t-tell-you-shutterstock_303995969-1024x683.jpg'
-}
-const SeaFood = {
-  uri: 'https://cdn.vox-cdn.com/thumbor/3m7uQhwDWw1XbhDFJN072xfu5LI=/0x0:2048x1365/1200x900/filters:focal(861x520:1187x846)/cdn.vox-cdn.com/uploads/chorus_image/image/59949587/Swift___Sons_Cold_Platter.0.jpg'
-}
-const ChineseFood = {
-  uri: 'https://deltaban.ir/wp-content/uploads/2018/10/veg.jpg'
-}
-const ItalianFood = {
-  uri: 'https://static.chaihona.ru/598525bcdc251.jpg'
-}
-const Desert = {
-  uri: 'https://www.askchefdennis.com/wp-content/uploads/2014/10/Fettuccine-Alfredo-2.jpg'
-}
-  const Vietnamese = {
-      uri: 'https://livingnomads.com/wp-content/uploads/2017/06/13/prawn-rice-paper-rolls-vietnamese-cuisine.jpg'
-  }
-  const Steak = {
-      uri: 'https://buffalorunranch.com/wp-content/uploads/2017/08/ribeye-1.jpg'
-  }
-  const Cafe = {
-      uri: 'https://media-cdn.tripadvisor.com/media/photo-s/0c/0e/61/91/cafe-amore.jpg'
-  }
+
+
 export default class ChooseFavorite extends Component {
 
   constructor(props) {
@@ -161,61 +139,60 @@ export default class ChooseFavorite extends Component {
         <FavorText FavorText ={'Please Choose Your Favorite Type of Cuisines'}/>
 
         <View style={styles.imageLayout}>
-          <View >
             <FoodPicture
               FoodPicture={JapaneseImag}
               FoodSelected={this.state.Japanese}
               onPress={() => {this.handlefavoriteType("Japanese");this.AddItemsToArray('Japanese');}}
             />
-            </View>
-            <View>
-              <FoodPicture
-                FoodPicture={MexicanImag}
-                FoodSelected={this.state.Mexican}
-                onPress={() => {this.handlefavoriteType("Mexican");this.AddItemsToArray('Mexican');}}
-              />
-            </View>
-          <View>
-            <FoodPicture
-              FoodPicture={AmricanImag}
-              FoodSelected={this.state.Amrican}
-              onPress={() => {this.handlefavoriteType("Amrican");this.AddItemsToArray('Amrican');}}/>
-          </View>
-          <View >
-            <FoodPicture
-              FoodPicture={FastFood}
-              FoodSelected={this.state.FastFood}
-              onPress={() => {this.handlefavoriteType("FastFood");this.AddItemsToArray('FastFood');}}/>
-          </View>
-          <View >
-            <FoodPicture
-              FoodPicture={SeaFood}
-              FoodSelected={this.state.SeaFood}
-              onPress={() => {this.handlefavoriteType("SeaFood");this.AddItemsToArray('SeaFood');}}/>
-          </View>
-          <View >
-            <FoodPicture
-              FoodPicture={ChineseFood}
-              FoodSelected={this.state.ChineseFood}
-              onPress={() => {this.handlefavoriteType("ChineseFood");this.AddItemsToArray('ChineseFood');}}/>
-          </View>
-          <View >
-            <FoodPicture
-              FoodPicture={ItalianFood}
-              FoodSelected={this.state.ItalianFood}
-              onPress={() => {this.handlefavoriteType("ItalianFood");this.AddItemsToArray('ItalianFood');}}/>
-          </View>
           <View>
             <FoodPicture
               FoodPicture={KoreanImag}
               FoodSelected={this.state.Korean}
               onPress={() => {this.handlefavoriteType("Korean");this.AddItemsToArray('Korean');}}/>
           </View>
+
           <View>
             <FoodPicture
-              FoodPicture={Desert}
-              FoodSelected={this.state.Desert}
-              onPress={() => {this.handlefavoriteType("Desert");this.AddItemsToArray('Desert');}}/>
+              FoodPicture={AmricanImag}
+              FoodSelected={this.state.Amrican}
+              onPress={() => {this.handlefavoriteType("Amrican");this.AddItemsToArray('Amrican');}}/>
+          </View>
+
+
+          <View >
+            <FoodPicture
+              FoodPicture={ItalianFood}
+              FoodSelected={this.state.ItalianFood}
+              onPress={() => {this.handlefavoriteType("ItalianFood");this.AddItemsToArray('ItalianFood');}}/>
+          </View>
+
+          <View>
+            <FoodPicture
+              FoodPicture={MexicanImag}
+              FoodSelected={this.state.Mexican}
+              onPress={() => {this.handlefavoriteType("Mexican");this.AddItemsToArray('Mexican');}}
+            />
+          </View>
+
+          <View >
+            <FoodPicture
+              FoodPicture={SeaFood}
+              FoodSelected={this.state.SeaFood}
+              onPress={() => {this.handlefavoriteType("SeaFood");this.AddItemsToArray('SeaFood');}}/>
+          </View>
+          
+          <View >
+            <FoodPicture
+              FoodPicture={FastFood}
+              FoodSelected={this.state.FastFood}
+              onPress={() => {this.handlefavoriteType("FastFood");this.AddItemsToArray('FastFood');}}/>
+          </View>
+
+          <View >
+            <FoodPicture
+              FoodPicture={ChineseFood}
+              FoodSelected={this.state.ChineseFood}
+              onPress={() => {this.handlefavoriteType("ChineseFood");this.AddItemsToArray('ChineseFood');}}/>
           </View>
           <View>
             <FoodPicture
@@ -223,18 +200,30 @@ export default class ChooseFavorite extends Component {
               FoodSelected={this.state.Vietnamese}
               onPress={() => {this.handlefavoriteType("Vietnamese");this.AddItemsToArray('Vietnamese');}}/>
           </View>
+
           <View>
             <FoodPicture
               FoodPicture={Steak}
               FoodSelected={this.state.Steak}
               onPress={() => {this.handlefavoriteType("Steak");this.AddItemsToArray('Steak');}}/>
           </View>
+
           <View>
             <FoodPicture
               FoodPicture={Cafe}
               FoodSelected={this.state.Cafe}
               onPress={() => {this.handlefavoriteType("Cafe");this.AddItemsToArray('Cafe');}}/>
           </View>
+
+          <View>
+            <FoodPicture
+              FoodPicture={Desert}
+              FoodSelected={this.state.Desert}
+              onPress={() => {this.handlefavoriteType("Desert");this.AddItemsToArray('Desert');}}/>
+          </View>
+          
+          
+          
         </View>
 
         <TouchableHighlight
